27ce05eb834efbd96e31dd2ed5c6658a68aeedf9
cwq2824 <ahmedkh949@gmail.com>
2023-05-23 20:21:45 +0200

    Final Submission of Basic Data Science Course
    Name# Muhammad Ahmed

---
+++ sales_analysis.R
@@ -0,0 +1,44 @@
+# Data Science at TUHH ------------------------------------------------------
+# SALES ANALYSIS ----
+
+# 1.0 Load libraries ----
+
+library(tidyverse)
+library(readxl)
+# 2.0 Importing Files ----
+bikes_tbl      <- read_excel(path ="C:/Users/muhammad.ahmed/OneDrive - AERQ GmbH/Documents/TUHH Start-up Engineering/Data Science/01_bike_sales/01_raw_data/bikes.xlsx")
+orderlines_tbl <- read_excel("C:/Users/muhammad.ahmed/OneDrive - AERQ GmbH/Documents/TUHH Start-up Engineering/Data Science/01_bike_sales/01_raw_data/orderlines.xlsx")
+bikeshops_tbl  <- read_excel("C:/Users/muhammad.ahmed/OneDrive - AERQ GmbH/Documents/TUHH Start-up Engineering/Data Science/01_bike_sales/01_raw_data/bikeshops.xlsx")
+# 3.0 Examining Data ----
+
+
+# 4.0 Joining Data ----
+
+
+# 5.0 Wrangling Data ----
+
+
+# 6.0 Business Insights ----
+# 6.1 Sales by Year ----
+
+# Step 1 - Manipulate
+
+# Step 2 - Visualize
+
+
+# 6.2 Sales by Year and Category 2 ----
+
+# Step 1 - Manipulate
+
+# Step 2 - Visualize
+
+
+
+# 7.0 Writing Files ----
+
+# 7.1 Excel ----
+
+# 7.2 CSV ----
+
+# 7.3 RDS ----
+
